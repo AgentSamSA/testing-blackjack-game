3159ae647db698853ca43c94cbb29055c1bc1577
Jordan Wade <oliver.jordan199616@gmail.com>
2021-11-05 17:57:33 -0400

    Player Game Flow

---
+++ src/test/java/com/example/testingblackjackgame/Flow/PlayerGameFlowTest.java
@@ -0,0 +1,54 @@
+package com.example.testingblackjackgame.Flow;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import com.example.testingblackjackgame.Deck.IConvertCardValue;
+import com.example.testingblackjackgame.Deck.IDealtACardToHand;
+import com.example.testingblackjackgame.Scoring.ICheckIfUnder22;
+import com.example.testingblackjackgame.Scoring.ISumPoints;
+
+import org.junit.Test;
+import org.mockito.Mock;
+
+
+
+
+public class PlayerGameFlowTest {
+    @Mock
+    IConvertCardValue mockConvertCardValue = mock(IConvertCardValue.class);
+
+    @Mock
+	ISumPoints mockSumPoints = mock(ISumPoints.class);
+	
+    @Mock
+    ICheckIfUnder22 mockCheckIfUnder22 = mock(ICheckIfUnder22.class);
+
+    
+
+
+    @Test
+	public void PlayerGameFlowTest() {
+
+		// Given: I am a user
+        given(mockConvertCardValue.getResult("A")).willReturn(11);
+		given(mockSumPoints.sum(new ArrayList<Integer>())).willReturn(2);
+		given(mockCheckIfUnder22.getResult(9)).willReturn(true); 
+	
+		// When: I Player a game
+		IPlayerGameFlow IPGF = new PlayerGameFlow(mockConvertCardValue, mockSumPoints, mockCheckIfUnder22);
+		IPGF.getResult();
+		
+		// Then: mockCreateDeck will be called one time
+		//		mockCheckIfUnder22 will be called one time
+		verify(mockConvertCardValue, times(2)).getResult("A");
+        verify(mockSumPoints, times(1)).sum(new ArrayList<Integer>());
+		verify(mockCheckIfUnder22, times(1)).getResult(9);
+
+	}
+}
